6bf81b8a600c30c92e4cf938cdde595d81f3db91
Anthony Batchelor <anthony.batchelor@examtime.com>
2012-08-23 15:32:18 +0100

    Convert error and exception strings to be internationalised

--- lib/ancestry.rb
+++ lib/ancestry.rb
@@ -3,6 +3,9 @@ require_relative 'ancestry/instance_methods'
 require_relative 'ancestry/exceptions'
 require_relative 'ancestry/has_ancestry'
 
+I18n.load_path += Dir[File.join(File.expand_path(File.dirname(__FILE__)),
+                                 'ancestry', 'locales', '*.{rb,yml}').to_s]
+
 module Ancestry
   ANCESTRY_PATTERN = /\A[0-9]+(\/[0-9]+)*\Z/
 end
